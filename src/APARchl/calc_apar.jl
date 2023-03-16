9d94f76d7738ec86f1d55df51286de6c0825f49f
Russell Doughty <russell.doughty@ou.edu>
2023-03-15 20:57:25 -0500

    working run for v0.2

--- src/APARchl/calc_apar.jl
+++ src/APARchl/calc_apar.jl
@@ -7,19 +7,47 @@
 #
 ###############################################################################
 
+function day_length(doy, lat)
+    _decl = 23.45 * sind(360 * (283 + doy) / 365);
+
+    if -tand(lat) * tand(_decl) <= -1
+        return 24
+    elseif -tand(lat) * tand(_decl) > 1
+        return 0
+    else
+        return 2 * acosd(-tand(lat) * tand(_decl)) / 15
+    end
+end
 
 function calc_apar(sif::String, y::String)
-    sif    = Dataset(sif)["sif743_qc"][:,:,:]
-    y      = Dataset(y)["sif_yield"][:,:,:]
+    sif_data = Dataset(sif)["sif743_qc"][:,:,:]
+    yield   = Dataset(y)["sif_yield"][:,:,:]
+    lat = Dataset(sif)["lat"][:]
+    
+    sif_data = replace!(sif_data, missing => NaN)
+    yield   = replace!(yield, missing => NaN)
 
-    # Arrange rasters dims to match
-    sif    = replace!(sif, missing => NaN)
-    y      = replace!(y, missing => NaN)
+    # Get day length correction factor
+    lat_array = similar(sif_data, Float32)
+    doy_array = similar(sif_data, Float32)
 
-    apar                = sif ./ y
+    # Build array of lats and doy
+    for i in 1:size(lat_array)[3]
+        for j in 1:size(lat_array)[1]
+            lat_array[j,:,i] = lat
+        end
+        doy_array[:,:,i] .= i * 8 - 4
+    end
+    doy_array[:,:,46] .= 363
+
+    day_factor = day_length.(doy_array, lat_array) ./ 24
+
+    apar                = sif_data ./ yield .* day_factor
     apar                = replace!(apar, missing => NaN)
     apar[apar .< 0]    .= NaN;
     apar[apar .== Inf] .= NaN;
+
+    # Arrange rasters dims to match
     apar                = permutedims(apar, [2,1,3])
 
     println("APARchl has been calculated.")
